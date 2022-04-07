2ac65408e429eacecd6f1379d0d4a9d38ff60126
Flavia Almeida <eufga1@gmail.com>
2022-04-07 13:38:17 -0300

    Task DIP

---
+++ 0x02/dip/MongoDBProductRepository.java
@@ -0,0 +1,27 @@
+public class MongoDBProductRepository implements IDbProduct {
+
+	@Override
+	public String getProductById(String productID) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String update(Product product) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String delete(String productID) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ 0x02/dip/MySQLProductRepository.java
@@ -0,0 +1,25 @@
+public class MySQLProductRepository implements IDbProduct {
+
+	public String getProductById(String productID) {
+		return null;
+	}
+
+	@Override
+	public String getAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String update(Product product) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String delete(String productID) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
