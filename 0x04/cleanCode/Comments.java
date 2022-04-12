51e6c34505773f252d4b06d3f3248ca30d29dd9c
Flavia Almeida <eufga1@gmail.com>
2022-04-12 20:30:01 -0300

    update task comments Clean Code

--- 0x04/cleanCode/Comments.java
+++ 0x04/cleanCode/Comments.java
@@ -9,6 +9,13 @@ public class Comments {
         return;
     }
 
+    /**
+     * <p> this method publishes results based on date
+     * </p>
+     * @param date date from when results should be published
+     * @return List of Result entities
+     * @since 16.0
+     */
     public List<Result> publishResults(Date date) {
        
     }
